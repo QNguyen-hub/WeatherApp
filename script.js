d87400cad7f346bd5488a633616a3b527e180525
Quy Nguyen <qnguyen303@gmail.com>
2022-10-20 23:55:55 -0600

    adding html and java

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+// var apiWeather = `https://api.openweathermap.org/data/2.5/onecall?${apiKey}`;
 var apiKey = "002ed83e9e4c82917cbc204e7a267650";
-var apiWeather = `https://api.openweathermap.org/data/2.5/onecall?${apiKey}`;
-var submit = document.querySelector("#submit");
+var weather = document.querySelector(".weather");
+// var submit = document.querySelector("#submit");
 
-submit.addEventListener("click", function () {
-  var city = document.querySelector("#city");
-  localStorage.setItem("city", city.value);
-  var location = localStorage.getItem("city");
-  var apiGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=840&appid=${apiKey}`;
-  fetch(apiGeo)
+var apiGeo = `http://api.openweathermap.org/geo/1.0/direct?q=Denver&limit=840&appid=002ed83e9e4c82917cbc204e7a267650`;
+// var city = document.querySelector("#city");
+var apiWeather = `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=${apiKey}`;
+
+document.getElementById("submit").addEventListener("click", (event) => {
+  event.preventDefault();
+  var city = document.getElementById("city").value;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+  )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data);
+      let lat = data.city.coord.lat;
+      let lon = data.city.coord.lon;
+      document.getElementById("current").innerHTML = data.city.coord.lat;
+      document.getElementById("cityName").innerHTML = `City: ${data.city.name}`;
+      document.getElementById(
+        "temp"
+      ).innerHTML = `Temp: ${data.list[0].main.temp}`;
+      document.getElementById("humidity").innerHTML = `Humidity:
+        ${data.list[0].main.humidity}`;
+      document.getElementById(
+        "wind"
+      ).innerHTML = `Wind: ${data.list[0].wind.speed}`;
+    });
 });
